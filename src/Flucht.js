5444a65891a384f3e97a1a458f96b00b11c30e9c
Max <maximilian.paulsen@gmail.com>
2016-09-22 21:43:50 -0700

    made preliminary tests with the pixi library will flush out further tomorrow and will absract away some of the "hacks" used to get this to work

--- src/Flucht.js
+++ src/Flucht.js
@@ -1,6 +1,26 @@
+import Runner from "./Runner.js";
+
 class Flucht{
+  constructor(){
+    this.stage = new PIXI.Container();
+    this.renderer = new PIXI.WebGLRenderer(800, 600);
+  }
   start(){
     console.log("the flucht has started. Da Da Daaa");
+    document.body.appendChild(this.renderer.view);
+  }
+
+  addRunner(){
+    this.runner = new Runner();
+    this.stage.addChild(this.runner.sprite);
+  }
+
+  render(){
+    this.renderer.render(this.stage);
+  }
+
+  update(){
+    this.runner.update();
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import Flucht from "./Flucht.js";
 var flucht = new Flucht();
-flucht.start();
+
+function animate(){
+  requestAnimationFrame(animate);
+  flucht.render();
+}
+
+setTimeout(update, 20);
+function update(){
+  setTimeout(update, 20);
+  flucht.update();
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  flucht.start();
+  flucht.addRunner();
+  animate();
+});
+
+window.keys = [];
+window.onkeyup = function(e) {keys[e.keyCode]=false;}
+window.onkeydown = function(e) {keys[e.keyCode]=true;}
