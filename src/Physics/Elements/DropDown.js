9e22413b65f18ed837337d582397ee9352f158bb
Javier <galvanjavier540@gmail.com>
2017-04-06 10:16:07 -0700

    Added JS doc to Element.js and started on DropDown.js

--- src/Physics/Elements/DropDown.js
+++ src/Physics/Elements/DropDown.js
@@ -1,10 +1,10 @@
 import Element from "./Element.js";
-
+/**class that creates "Drop Down" type element*/
 class DropDown extends Element{
   constructor(x, y, width, height){
     super(x, y, width, height, "Drop Down");
   }
-
+  
   collision(entity, side, entityX, entityY){
     if(side === 2){
       let relFeetHeight = entityY - entity.box.height/2 - this.pos.y;
--- src/Physics/Elements/Element.js
+++ src/Physics/Elements/Element.js
@@ -1,5 +1,5 @@
 import Box from "./../Box.js";
-
+/**class representing physical element in space*/
 class Element{
   constructor(x, y, width, height, type = "Element", renderAsBox = true){
     this.pos = {x:x, y:y};
@@ -7,13 +7,17 @@ class Element{
     this.type = type;
     this.renderAsBox = renderAsBox;
   }
-
+  /**
+  * empty method called when collision occurs and returns which side collided
+  * @param entity object that collides with this
+  * @param side side where the collision occurs
+  */
   collision(entity, side){
 
   }
-
+  /** empty method called when updating*/
   update(){
-    
+
   }
 }
 
