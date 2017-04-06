b86309a8e81756b8b73713670690f70c84e49b95
Javier <galvanjavier540@gmail.com>
2017-04-06 12:14:05 -0700

    added jsDocs to DropDown.js

--- src/Physics/Elements/DropDown.js
+++ src/Physics/Elements/DropDown.js
@@ -1,10 +1,26 @@
 import Element from "./Element.js";
 /**class that creates "Drop Down" type element*/
 class DropDown extends Element{
+  /**
+  * creates DropDown object by calling Element Constructor with type "Drop Down"
+  * @constructor
+  * @param {number} x the x-coordinate of Drop Down element
+  * @param {number} y the y-coordinate of Drop Down element
+  * @param {number} width width of the element
+  * @param {number} height height of the element
+  */
   constructor(x, y, width, height){
     super(x, y, width, height, "Drop Down");
   }
-  
+
+  /**
+  * checks if there is a collision on specific side of the element
+  * @param {Entity} entity the entity that collides with this
+  * @param {number} side the side where collision occurs
+  * @param {number} entityX the x-coordinate of the entity
+  * @param {number} entityY the y-coordinate of the entity
+  * @returns {boolean} returns true if collision occurs on any side other than 2
+  */
   collision(entity, side, entityX, entityY){
     if(side === 2){
       let relFeetHeight = entityY - entity.box.height/2 - this.pos.y;
