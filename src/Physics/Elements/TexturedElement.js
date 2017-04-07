0871da5d631d4aed3ac6facdb2f61732a444f029
Javier <galvanjavier540@gmail.com>
2017-04-07 10:07:06 -0700

    Added JsDocs to TexturedElement.js

--- src/Physics/Elements/TexturedElement.js
+++ src/Physics/Elements/TexturedElement.js
@@ -1,6 +1,15 @@
 import Element from "./Element.js";
-/** Creates an element that is type "TexturedElement" */
+/** Creates an element that is type "TexturedElement" that exists to display an image*/
 class TexturedElement extends Element{
+  /**
+  * Creates Textured Element using element constructor and setting type to "Textured Element"
+  * @constructor
+  * @param {number} x the x-coordinates of the Textured Element
+  * @param {number} y the y-coordinate of the Textured Element
+  * @param {number} width the width of the Textured Element
+  * @param {number} height the height of the Textured Element
+  * @param {ElementDescription} element the description of the element that the image is displayed on
+  */
   constructor(x, y, width, height, element){
     super(x, y, width, height, "Textured Element", false);
     this.state = "idle";
@@ -9,7 +18,13 @@ class TexturedElement extends Element{
     this.offX = element.offsetX;
     this.ghost = element.ghost;
   }
-
+/**
+* Empty method that is called to check if collision occurs
+* @param {Entity} entity the entity that this collides with
+* @param {number} side the side where collision occurs
+* @param {number} entityX the x-coordinate of the entity that is collided with
+* @param {number} entityY the y-coordinate of the entity that is collided with
+*/
   collision(entity, side, entityX, entityY){
 
   }
