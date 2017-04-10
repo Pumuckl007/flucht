4348da5c2508612e4e0a733ecaef3e6f479865d8
Pumuckl007 <maximilian.paulsen@gmail.com>
2017-04-10 14:13:43 -0700

    added JSDoc for the world

--- src/Physics/World.js
+++ src/Physics/World.js
@@ -1,7 +1,16 @@
 import Collisions from "./Collisions.js";
 import Terrain from "./Terrain.js";
 
+/**
+* The class representing the entier world for physics purposes
+*/
 class World{
+
+  /**
+  * creates a new world that loads the level "/levels/Level1.json" and calls the spawn handler when done as specified in Terrain constructor.
+  * @param {Object} spawnHandler the spawn handler to have spawn runner called on
+  * @param {Function} spawnHandler.spawnRunner a method that should spawn a runner
+  */
   constructor(spawnHandler){
     this.entities = [];
     this.tickingEntities = [];
@@ -9,10 +18,19 @@ class World{
     this.listeners = [];
   }
 
+  /**
+  * adds the event listener to the pool of listeners
+  * @param {Object} listener the listener
+  * @param {Function} listener.onEvent the function called when an even occures, called with a string and an update object.
+  */
   addEventListener(listener){
     this.listeners.push(listener);
   }
 
+  /**
+  * adds a new entity to the world
+  * @param {Entity} entity the entity to add
+  */
   addEntity(entity){
     this.entities.push(entity);
     if(entity.needsTick){
@@ -25,7 +43,8 @@ class World{
   }
 
   /**
-  * @peram timestep the part of one second passed
+  * called when a tick occures
+  * @param timestep the part of one second passed
   */
   tick(timestep){
     for(let entity of this.tickingEntities){
