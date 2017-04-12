4127db2acfa7311566ebd812d72d46a72f1807b6
Javier <galvanjavier540@gmail.com>
2017-04-11 17:37:43 -0700

    added JsDocs to Party.js

--- src/Party.js
+++ src/Party.js
@@ -1,8 +1,18 @@
+/** Class representing the users that are in the current session*/
 class Party{
+  /**
+  * adds the first user in the Party
+  * @constructor
+  * @param {User} user the user to be added
+  */
   constructor(user){
     this.users = [user];
   }
 
+  /**
+  * removes user from the Party
+  * @param {String} id the id of the user to be removed
+  */
   removeUser(id){
     let index = -1;
     for(let i = 0; i<this.users.length && index === -1; i++){
@@ -15,11 +25,19 @@ class Party{
     }
   }
 
+  /**
+  * adds user to the Party
+  * @param {User} user the user to be added
+  */
   addUser(user){
     console.log("Added", user)
     this.users.push(user);
   }
 
+  /**
+  * updates the party string of current users
+  * @param {?} domElement ????
+  */
   update(domElement){
     let string = "";
     for(let user of this.users){
