466b59790aa6cc31fa26625546511553040e4768
Javier <galvanjavier540@gmail.com>
2017-04-17 10:03:14 -0700

    Added the light and light source class

---
+++ src/Physics/LightSource.js
@@ -0,0 +1,33 @@
+import Entity from "./Entity.js";
+
+/** Class representing light source entity that moves and follows other Enities and textures*/
+class LightSource extends Entity{
+  /**
+  * Creates the light source with sprite to follow
+  * @constructor
+  * @param {number} width the width of the light
+  * @param {number} height the height of the light
+  * @param {Object}  object Entity or element containing the position of the sprite
+  * @param {boolean} moves true if the light needs to move to follow sprite
+  */
+  constructor(width, height, object, moving){
+    super(width, height);
+    this.follow = object;
+    this.hasPhysics = moving; //calls update method
+    this.needsTick = false; //calls tick method
+    this.worldCollisions = false;
+    this.type = "LightSource";
+    this.pos = {x:this.follow.pos.x, y:this.follow.pos.y};
+  }
+
+  /**
+  * update location of light
+  * @param timestep the part of one second passed
+  */
+  update(timestep){
+    this.pos.x = this.follow.pos.x;
+    this.pos.y = this.follow.pos.y;
+  }
+}
+
+export default LightSource;
